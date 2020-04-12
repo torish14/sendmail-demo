b81103d50371100aaf1612db8175cc9ba4eac8ec
toshihirokato <nanamin.104@icloud.com>
2020-04-12 22:00:06 +0900

    カードコンポーネントの追加

---
+++ src/components/Card1.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- <v-layout> -->
+      <!-- <v-flex> -->
+    <!-- <v-container> -->
+    <v-card
+      :elevation="20"
+      class="mx-auto"
+      max-width="400"
+      style="right: 25px;"
+    >
+    <v-img
+      class="white--text align-end"
+      height="250px"
+      width="500px"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYqjSvZihh0pM6D-5T-ePmu3ByhHsqPjZ2fbpoVic92zGXr8Zs&s"
+      style="opacity: 0.8;"
+    >
+      <v-card-title>
+        Interview 2 million successful people <br>about success factors
+      </v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0" style="font-size: 18px;">
+      Step1
+    </v-card-subtitle>
+
+    <v-card-text class="text--primary" style="font-size: 15px;">
+      <div><br>The development of the Strengths Finder started<br> an interview with 2 million people who unconsciously explored what "talents" were being used when one was performing.</div>
+    </v-card-text>
+    </v-card>
+    <!-- </v-container> -->
+    <!-- </v-flex> -->
+    <!-- </v-layout> -->
+  </div>
+</template>
