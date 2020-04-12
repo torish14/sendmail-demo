fb32a8faf66ecb1d7a52e96dfe1603291adfb385
toshihirokato <nanamin.104@icloud.com>
2020-04-12 21:46:04 +0900

    Themes1コンポーネントの作成

---
+++ src/components/Themes1.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="display-2 font-weight-thin">
+      My themes
+    </h1>
+
+    <v-container fluid>
+      <v-row>
+        <v-col>
+          <v-img src="../assets/Clifton DNA.png"></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
