8ed58d97cf26d237602948c287d84dc16c478369
toshihirokato <nanamin.104@icloud.com>
2020-04-12 22:16:03 +0900

    Themesコンポーネントの追加

---
+++ src/components/Themes.vue
@@ -0,0 +1,125 @@
+<template>
+  <!-- <div class="image" style="position: relative; bottom: 80px; left: 80px; height: 900px;"> -->
+    <div>
+      <h1 class="display-2 font-weight-thin">My themes</h1>
+    <!-- <v-hover v-slot:default="{ hover }" close-delay="200">
+    <v-card :elevation="hover ? 16 : 2" max-width="990"> -->
+    <img src="../assets/Clifton DNA.png" alt="" style="">
+    <!-- </v-card>
+    </v-hover> -->
+    <div class="strengthen" style="position: relative; top: 20px;">
+      <p><strong style="font-size: 14px;">Strengthen</strong></p>
+      <p><strong style="border-left: solid 4px rgba(255, 204, 0, 1); padding-left: 8px;">1. Self-Assurance</strong></p>
+      <p><strong style="border-left: solid 4px rgba(255, 204, 0, 1); padding-left: 8px;">2. Command</strong></p>
+      <p><strong style="border-left: solid 4px rgba(255, 204, 0, 1); padding-left: 8px;">3. Competition</strong></p>
+      <p><strong style="border-left: solid 4px rgba(153, 51, 102, 1); padding-left: 8px;">4. Ideation</strong></p>
+      <p><strong style="border-left: solid 4px rgba(153, 51, 102, 1); padding-left: 8px;">5. Strategic</strong></p>
+      <p style="border-left: solid 4px rgba(68, 0, 0, 1); padding-left: 8px;">
+        6. Belief
+      </p>
+      <p style="border-left: solid 4px rgba(153, 51, 102, 1); padding-left: 8px;">
+        7. Futuristic
+      </p>
+      <p style="border-left: solid 4px rgba(255, 204, 0, 1); padding-left: 8px;">
+        8. Activator
+      </p>
+      <p style="border-left: solid 4px rgba(255, 204, 0, 1); padding-left: 8px;">
+        9. Maximizer
+      </p>
+      <p style="border-left: solid 4px rgba(68, 0, 0, 1); padding-left: 8px;">
+        10. Achiever
+      </p>
+    </div>
+    <div class="confirm1" style="position: relative; top: -420px; left: 280px;">
+      <p><strong style="font-size: 14px;">Confirm</strong></p>
+      <p style="border-left: solid 4px rgba(255, 204, 0, 1); padding-left: 8px;">
+        11. Woo
+      </p>
+      <p style="border-left: solid 4px rgba(68, 0, 0, 1); padding-left: 8px;">
+        12. Focus
+      </p>
+      <p style="border-left: solid 4px rgba(0, 51, 51, 1); padding-left: 8px;">
+        13. Connectedness
+      </p>
+      <p style="border-left: solid 4px rgba(0, 51, 51, 1); padding-left: 8px;">
+        14. Relator
+      </p>
+      <p style="border-left: solid 4px rgba(153, 51, 102, 1); padding-left: 8px;">
+        15. Learner
+      </p>
+      <p style="border-left: solid 4px rgba(153, 51, 102, 1); padding-left: 8px;">
+        16. Intellection
+      </p>
+      <p style="border-left: solid 4px rgba(0, 51, 51, 1); padding-left: 8px;">
+        17. Individualization
+      </p>
+      <p style="border-left: solid 4px rgba(68, 0, 0, 1); padding-left: 8px;">
+        18. Responsibility
+      </p>
+      <p style="border-left: solid 4px rgba(153, 51, 102, 1); padding-left: 8px;">
+        19. Input
+      </p>
+      <p style="border-left: solid 4px rgba(255, 204, 0, 1); padding-left: 8px;">
+        20. Significance
+      </p>
+    </div>
+    <div class="confirm2" style="position: relative; top: -820px; left: 560px;">
+      <p style="border-left: solid 4px rgba(0, 51, 51, 1); padding-left: 8px;">
+        21. Positivity
+      </p>
+      <p style="border-left: solid 4px rgba(0, 51, 51, 1); padding-left: 8px;">
+        22. Includer
+      </p>
+      <p style="border-left: solid 4px rgba(255, 204, 0, 1); padding-left: 8px;">
+        23. Communication
+      </p>
+      <p style="border-left: solid 4px rgba(153, 51, 102, 1); padding-left: 8px;">
+        24. Analytical
+      </p>
+      <p style="border-left: solid 4px rgba(0, 51, 51, 1); padding-left: 8px;">
+        25. Developer
+      </p>
+      <p style="border-left: solid 4px rgba(0, 51, 51, 1); padding-left: 8px;">
+        26. Empathy
+      </p>
+      <p style="border-left: solid 4px rgba(68, 0, 0, 1); padding-left: 8px;">
+        27. Arranger
+      </p>
+      <p style="border-left: solid 4px rgba(0, 51, 51, 1); padding-left: 8px;">
+        28. Adaptability
+      </p>
+      <p style="border-left: solid 4px rgba(68, 0, 0, 1); padding-left: 8px;">
+        29. Discipline
+      </p>
+      <p style="border-left: solid 4px rgba(153, 51, 102, 1); padding-left: 8px;">
+        30. Context
+      </p>
+    </div>
+    <div class="confirm3" style="position: relative; top: -1220px; left: 840px;">
+      <p style="border-left: solid 4px rgba(68, 0, 0, 1); padding-left: 8px;">
+        31. Restorative
+      </p>
+      <p style="border-left: solid 4px rgba(68, 0, 0, 1); padding-left: 8px;">
+        32. Deliberative
+      </p>
+      <p style="border-left: solid 4px rgba(68, 0, 0, 1); padding-left: 8px;">
+        33. Consistency
+      </p>
+      <p style="border-left: solid 4px rgba(0, 51, 51, 1); padding-left: 8px;">
+        34. Harmony
+      </p>
+    </div>
+
+  </div>
+  <!-- </div> -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
