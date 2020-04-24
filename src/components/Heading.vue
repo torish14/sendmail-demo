fbbaaaa6de5391200df534300e04ec0b424e2236
toshihirokato <nanamin.104@icloud.com>
2020-04-24 12:15:33 +0900

    Headingコンポーネントの追加

---
+++ src/components/Heading.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mt-12 ml-2">
+    <h1 class="display-2 font-regular">Strengths</h1>
+    <h2 class="display-2 font-regular">Finder</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.mt-12 {
+  position: relative;
+  top: 48px;
+}
+</style>
