90cf286c2a53bee551232bf8a39a869d24287c22
toshihirokato <nanamin.104@icloud.com>
2020-04-24 12:29:18 +0900

    sectionタグの追加

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-app>
-    <Navbar />
-    <Heading />
-    <Bubble1 class="bubble1" />
-    <Cards />
-    <Bubble2 class="bubble2 ml-12" />
-    <Panels />
-    <Bubble3 class="bubble3 ml-20" />
-    <Themes />
-    <Bubble4 class="bubble4 ma-10" />
-    <ContactForm class="my-12" />
-  </v-app>
+  <section class="container">
+    <v-app>
+      <Navbar />
+      <Heading />
+      <Bubble1 class="bubble1" />
+      <Cards />
+      <Bubble2 class="bubble2 ml-12" />
+      <Panels />
+      <Bubble3 class="bubble3 ml-20" />
+      <Themes />
+      <Bubble4 class="bubble4 ma-10" />
+      <ContactForm class="my-12" />
+    </v-app>
+  </section>
 </template>
 
 <script>
